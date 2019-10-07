90b2f4879b631a8dfc5e31d3c1651125b2eaa370
ilolivie <54978914+ilolivie@users.noreply.github.com>
2019-10-07 20:38:39 +0300

    задание 2

--- client.py
+++ client.py
@@ -3,14 +3,18 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('localhost', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
+print("Введите сообщение./nЕсли хотите завершить работу с сервером, введите exit.")
+msg = ""
+while True:
+    if msg != 'exit':
+        msg = input()
+        sock.send(msg.encode())
+        data = sock.recv(1024)
+    else:
+        break
 
 sock.close()
 
-print(data.decode())
+print("Работа завершена.")
