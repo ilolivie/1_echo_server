b53d84632cd2bbb340c26e80313fadbd2376f4a7
ilolivie <54978914+ilolivie@users.noreply.github.com>
2019-10-07 20:57:51 +0300

    задание 3

--- client.py
+++ client.py
@@ -5,16 +5,16 @@ sock = socket.socket()
 sock.setblocking(1)
 sock.connect(('localhost', 9090))
 
-print("Введите сообщение./nЕсли хотите завершить работу с сервером, введите exit.")
+print("Введите сообщение.Если хотите завершить работу с сервером, введите exit.")
 msg = ""
 while True:
     if msg != 'exit':
         msg = input()
         sock.send(msg.encode())
-        data = sock.recv(1024)
+        #data = sock.recv(1024)
     else:
         break
 
 sock.close()
 
-print("Работа завершена.")
+#print("Работа завершена.")
--- server.py
+++ server.py
@@ -2,19 +2,17 @@ import socket
 
 sock = socket.socket()
 sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
+sock.listen()
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += ' ' + data.decode()
-	conn.send(data)
+    conn, addr = sock.accept()
+    print(addr)
+    msg = ''
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print(data.decode())
 
-print(msg)
 
 conn.close()
