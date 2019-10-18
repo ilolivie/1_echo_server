ac7e13981d5dc69fc5459b48106715ccdeea5749
ilolivie <54978914+ilolivie@users.noreply.github.com>
2019-10-18 11:38:16 +0300

    теперь работает

--- server.py
+++ server.py
@@ -2,20 +2,26 @@ import socket
 
 sock = socket.socket()
 sock.bind(('', 9090))
-sock.listen(0)
+sock.listen()
 
-while True:
-    conn, addr = sock.accept()
-    print(addr)
-    msg = ''
+
+conn, addr = sock.accept()
+print(addr)
+msg = ''
+IsRunning = True    
+while IsRunning:
     
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        msg+= " " + data.decode()
-        conn.send(data)
+    data = conn.recv(1024)
+    
+    msg = data.decode()
+    if msg == 'exit':
+        IsRunning = False
+    else:
+        print(msg)
+        conn.send(msg.encode())
         
-    conn.close()
+conn.close()
     
-    print('конец')
+    
+    
+print('конец')
