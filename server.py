d12ac7dace9782ac488e52c04a91d90323ccfcd6
ilolivie <54978914+ilolivie@users.noreply.github.com>
2019-10-31 18:48:14 +0300

    задание 6

--- server.py
+++ server.py
@@ -3,10 +3,33 @@ import logging as log
 
 log.basicConfig(filename= 'file.log', format='%(levelname)s %(asctime)s - %(message)s', datefmt='%d.%m.%Y %H:%M:%S', level=log.INFO)
 log.info('Соединение установлено.')
-
+    
 sock = socket.socket()
-sock.bind(('', 9090))
-log.info('Сервер подключился к порту - 9090')
+
+try:
+	port=int(input("Ваш порт: "))
+	if not 0 <= port <= 65535:
+		raise ValueError
+except ValueError:
+		port = 9090
+        
+
+def check_port(port):
+    while True:
+        try: 
+            sock.bind(('', port))
+            break
+        except:
+            port+=1
+    log.info(f'Занял порт {port}.') 
+    print(f'Занял порт {port}.')
+    
+check_port(port)
+    
+
+
+
+log.info(f'Сервер подключился к порту - {port}')
 sock.listen()
 log.info('Сервер ожидает подключения ...')
 
@@ -18,7 +41,7 @@ IsRunning = True
 while IsRunning:
     
     data = conn.recv(1024)
-    msg = data.decode()
+    msg = data.decode() 
     
     log.info('Сервер отправляет клиенту ответ...')
     if msg == 'exit':
